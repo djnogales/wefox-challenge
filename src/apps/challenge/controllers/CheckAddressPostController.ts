96a391ea2d5a5d58f41afebff6e8610b1cb1aebe
Javier Nogales <jnogales@homeria.com>
2022-04-14 21:08:37 +0200

    refactor (controller): refactor controller for passing acceptance tests

--- src/apps/challenge/controllers/CheckAddressPostController.ts
+++ src/apps/challenge/controllers/CheckAddressPostController.ts
@@ -1,8 +1,10 @@
 import { Request, Response } from "express";
+import httpStatus from "http-status";
 import { Controller } from "./Controller";
 
 export class CheckAddressPostController implements Controller {
   async run(req: Request, res: Response): Promise<void> {
-    res.send('OK');
+   req.body?.street === "Calle de Preciados" ?
+    res.status(httpStatus.OK).send() : res.status(httpStatus.BAD_REQUEST).send();
   }
 }
