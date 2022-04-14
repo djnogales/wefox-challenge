b56cb58e4dd2455a30f1647eee2cdcc936eb2ebb
Javier Nogales <jnogales@homeria.com>
2022-04-15 00:41:47 +0200

    test (unit-tests) : implement address validator unit test

--- src/apps/challenge/controllers/CheckAddressPostController.ts
+++ src/apps/challenge/controllers/CheckAddressPostController.ts
@@ -1,10 +1,8 @@
 import { Request, Response } from "express";
-import httpStatus from "http-status";
 import { Controller } from "./Controller";
 
 export class CheckAddressPostController implements Controller {
   async run(req: Request, res: Response): Promise<void> {
-   req.body?.street === "Calle de Preciados" ?
-    res.status(httpStatus.OK).send() : res.status(httpStatus.BAD_REQUEST).send();
+    throw new Error("Method not implemented");
   }
 }
---
+++ tests/Contexts/Address/application/AddressValidator.test.ts
@@ -0,0 +1,22 @@
+import { AddressValidator } from "../../../../src/Contexts/Challenge/Address/application/AddressValidator";
+import { Address } from "../../../../src/Contexts/Challenge/Address/domain/Address";
+import { AddressRepository } from "../../../../src/Contexts/Challenge/Address/domain/AddressRepository";
+
+
+describe('AddressValidator', () => {
+  it('should return true with a valid address', async() => {
+    const repository: AddressRepository = {
+      search: jest.fn().mockReturnValue(true)
+    }
+    const validator = new AddressValidator(repository);
+    const street = "Calle de Preciados";
+    const streetNumber = "40";
+    const town = "Madrid";
+    const postalCode = "28013";
+    const country = "Spain";
+
+    await validator.run(street, streetNumber, town, postalCode, country);
+
+    expect(repository.search).toHaveBeenCalledWith(street, streetNumber, town, postalCode, country);
+  });
+});
